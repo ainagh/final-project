06cbf909ee11dbbd5214ac33bc1e27460b231b41
Aina Gatell Hijazo <aina.gatell@gmail.com>
2023-02-08 12:54:15 +0100

    sign in

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -5,21 +5,57 @@
   <div class="container">
     <h3 class="header-title">Log In to ToDo App</h3>
     <p class="header-subtitle">Estamos en la ruta de login. Aquí deberíais crear un form con la lógica correspondiente para que este permita al usuario loguearse con su email y su contraseña. Miraros la lógica de SignUp si necesitáis inspiración :)</p>
-    <p>Dont have an account? <PersonalRouter :route="route" :buttonText="buttonText" class="sign-up-link"/></p>
+
+   <div class="sign-in-form">
+    <form @submit.prevent="signIn">
+    <input type="email" class=""
+            placeholder="example@gmail.com"
+            id="email"
+            v-model="email"
+            required/> <br>
+    <input type="password"
+            class=""
+            placeholder="**********"
+            id="password"
+            v-model="password"
+            required/> <br>
+    <button class="button" type="submit">Sign In</button>
+    <p>Don't have an account? <PersonalRouter :route="route" :buttonText="buttonText" class="sign-up-link"/></p>
+  </form>
+  </div>
   </div>
 
 </template>
 
 <script setup>
+import { ref } from "vue";
 import PersonalRouter from "./PersonalRouter.vue";
+import { useRoute, useRouter } from "vue-router";
+import { useUserStore } from "../stores/user";
 
 // Route Variables
 const route = "/auth/signup";
 const buttonText = "Sign Up";
 
 // Arrow function to Signin user to supaBase
-const signIn = async () => {
+/* const signIn = async () => {
   try {} catch (error) {}
+}; */
+const redirect = useRouter();
+const email = ref("");
+const password = ref("");
+
+const signIn = async () => {
+  console.log("first")
+  if (email.value) {
+  try {
+    await useUserStore().signIn(email.value, password.value)
+    redirect.push ({ path: "/"})
+
+  } catch (error) {
+    throw error;
+  }
+  }
 };
 </script>
 
